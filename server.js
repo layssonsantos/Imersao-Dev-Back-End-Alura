bd5bebc458a2d5a4eb88f6d2985dbd3434311490
layssonsantos <laysson.nvt@gmail.com>
2024-11-20 19:20:52 -0300

    Definição de rotas, modulos e controlers

--- server.js
+++ server.js
@@ -1,46 +1,10 @@
-import express from 'express';
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
-// Objetos
-const posts = [
-    {
-        descricao: 'Uma foto teste', 
-        imagem: 'https://placecats.com/millie/300/150',
-        id: 1
-    },
-    {
-        descricao: 'Paisagem incrível!',
-        imagem: 'https://source.unsplash.com/random/300x150/?nature',
-        id: 2
-    },
-    {
-        descricao: 'Meu pet de estimação',
-        imagem: 'https://placekitten.com/300/200',
-        id: 3
-    }
-];
-
-// Criação do servidor
 const app = express();
 
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
-    console.log('Servidor escutando...');
-});
-
-// Criação de requisições
-app.get('/posts', (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorID(id) {
-    return posts.findIndex((posts) => {
-        return posts.id == Number(id)
-    })
-}
-
-// Get por ID
-app.get('/posts/:id', (req, res) => {
-    const index = buscarPostPorID(req.params.id);
-    res.status(200).json(posts[index]);
+    console.log("Servidor escutando...");
 });
